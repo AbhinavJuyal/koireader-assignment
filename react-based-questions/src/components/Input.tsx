b58b6994b5cd0a51f2fb41c472e210b78767b78e
Abhinav Juyal <52655310+AbhinavJuyal@users.noreply.github.com>
2023-01-18 11:08:31 +0530

    changes

--- react-based-questions/src/components/Input.tsx
+++ react-based-questions/src/components/Input.tsx
@@ -1,14 +1,29 @@
 import { XCircle } from "phosphor-react";
 
 interface IInput {
+  input: any;
   decInput: React.MouseEventHandler;
+  inputChange: (value: string, id: number) => void;
 }
 
-const Input = ({ decInput }: IInput) => {
+const Input = ({ input, decInput, inputChange }: IInput) => {
+  const [id, value] = input;
+  const handleOnChange: React.ChangeEventHandler = (e) => {
+    const { value, dataset } = e.target as HTMLInputElement;
+    const id = dataset.id;
+    inputChange(value, Number(id));
+  };
   return (
     <div className="flex items-center justify-between mb-2">
-      <input className="px-2 py-2" placeholder="type here!" type="text" />
-      <button type="button" onClick={decInput}>
+      <input
+        data-id={id}
+        value={value}
+        onChange={handleOnChange}
+        className="px-2 py-2"
+        placeholder="type here!"
+        type="text"
+      />
+      <button type="button" onClick={decInput} data-id={id}>
         <XCircle className="text-red-600" size={32} weight="fill" />
       </button>
     </div>
--- react-based-questions/src/components/Question3.tsx
+++ react-based-questions/src/components/Question3.tsx
@@ -5,14 +5,66 @@ import Input from "./Input";
 const maxInputs: number = 10;
 const minInputs: number = 0;
 
+const obj = [
+  {
+    id: 1,
+    value: "",
+  },
+  {
+    id: 2,
+    value: "",
+  },
+  {
+    id: 3,
+    value: "",
+  },
+  {
+    id: 4,
+    value: "",
+  },
+  {
+    id: 5,
+    value: "",
+  },
+];
+
+const obj1: Record<string, string> = {
+  1: "",
+  2: "",
+  3: "",
+  4: "",
+};
+
+const randomize = () => Math.floor(Math.random() * 1000);
+
 const Question3 = () => {
   const [totalInputs, setTotalInputs] = useState<number>(minInputs);
+  const [formData, setFormData] = useState(obj1);
 
-  const incInput: React.MouseEventHandler = () =>
-    setTotalInputs((prev) => (prev + 1 <= maxInputs ? prev + 1 : prev));
+  // const incInput: React.MouseEventHandler = () =>
+  //   setTotalInputs((prev) => (prev + 1 <= maxInputs ? prev + 1 : prev));
 
-  const decInput: React.MouseEventHandler = () =>
-    setTotalInputs((prev) => (prev - 1 >= minInputs ? prev - 1 : prev));
+  // const decInput: React.MouseEventHandler = () =>
+  //   setTotalInputs((prev) => (prev - 1 >= minInputs ? prev - 1 : prev));
+
+  const incInput: React.MouseEventHandler = (e) => {
+    setFormData((prev) => ({ ...prev, [randomize()]: "" }));
+  };
+
+  const decInput: React.MouseEventHandler = (e) => {
+    const { dataset } = e.currentTarget as HTMLButtonElement;
+    const id = dataset.id as string;
+    setFormData((prev) => {
+      const newObj = { ...prev };
+      delete newObj[id];
+      console.log(newObj);
+      return newObj;
+    });
+  };
+
+  const inputChange = (value: string, id: number) => {
+    setFormData((prev) => ({ ...prev, [id]: value }));
+  };
 
   return (
     <div className="max-w-[250px]">
@@ -23,8 +75,13 @@ const Question3 = () => {
         </button>
       </div>
       <div className="">
-        {new Array(totalInputs).fill(0).map((e, idx) => (
-          <Input key={idx} decInput={decInput} />
+        {Object.entries(formData).map((input, idx) => (
+          <Input
+            key={idx}
+            input={input}
+            decInput={decInput}
+            inputChange={inputChange}
+          />
         ))}
       </div>
     </div>
